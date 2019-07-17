1296a163fab4fa75c05943460aa1b3f4f6612e42
Evin Dunn <evindunn14@gmail.com>
2019-07-16 20:02:38 -0700

    Header scrolling updates

--- js/header.js
+++ js/header.js
@@ -8,24 +8,38 @@ jQuery(() => {
 function headerMain() {
   const header = $("#" + headerId);
   const logo = $("#" + logoId);
+  let currentScrollPos = 0;
+  let prevScrollPos = 0;
 
   $(window).scroll(() => {
-    if ($(window).scrollTop() >= 80 && !header.hasClass("site-header-scroll")) {
+    currentScrollPos = document.body.scrollTop || document.documentElement.scrollTop;
+
+    // Is moving downward and past 80px and isn't already collapsed
+    if ((currentScrollPos - prevScrollPos) > 5 && currentScrollPos > 80 && !header.hasClass("site-header-scroll")) {
       header.addClass("site-header-scroll");
       collapseSiteLogo(logo);
-    } else if ($(window).scrollTop() < 80 && header.hasClass("site-header-scroll")) {
+    // Is moving upward and past 80px and isn't already expanded
+    } else if ((currentScrollPos - prevScrollPos) < -5 && currentScrollPos < 80 && header.hasClass("site-header-scroll")) {
       header.removeClass("site-header-scroll");
+      $(document.body).animate({ scrollTop: 0 }, "slow");
       expandSiteLogo(logo);
     }
+
+    prevScrollPos = currentScrollPos;
   });
+
 }
 
 function collapseSiteLogo(logo) {
   const smallLogoPath = logo.attr("src").replace("oregonflora-logo.png", "oregonflora-logo-sm.png");
-  logo.attr("src", smallLogoPath);
+  logo.stop().animate({ opacity: 0 }, 100, () => {
+    logo.attr("src", smallLogoPath).animate({ opacity: 100 }, 100);
+  });
 }
 
 function expandSiteLogo(logo) {
   const lgLogoPath = logo.attr("src").replace("oregonflora-logo-sm.png", "oregonflora-logo.png");
-  logo.attr("src", lgLogoPath);
+  logo.stop().animate({ opacity: 0 }, 100, () => {
+    logo.attr("src", lgLogoPath).animate({ opacity: 100 }, 100);
+  });
 }
