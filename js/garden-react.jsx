b44916a707334bee2485de64c7160a51c4823e5e
Evin Dunn <evindunn14@gmail.com>
2019-09-04 11:19:12 -0700

    Main components in react

--- js/garden-react.jsx
+++ js/garden-react.jsx
@@ -16,22 +16,15 @@ class InfographicDropdown extends React.Component {
     return (
       <div
         id="infographic-dropdown"
-        className="container-fluid">
+        className="container-fluid p-5 d-print-none">
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "75% 25%",
-            gridAutoFlow: "column",
-            gridGap: "2em",
-            padding: "3em"
-          }}
-        >
-          <div>
-            <h1 style={{ width: this.state.isCollapsed ? "100%" : "50%", fontWeight: "bold" }}>
+        <div className="row">
+          <div className="col">
+            <h1
+              style={{ fontWeight: "bold", width: "80%" }}>
               Choose native plants for a smart, beautiful and truly Oregon garden
             </h1>
-            <h3 className={ "will-collapse" + (this.state.isCollapsed ? " is-collapsed" : "") }>
+            <h3 className={ "w-50 will-collapse" + (this.state.isCollapsed ? " is-collapsed" : "") }>
               Native plants thrive in Oregon’s unique landscapes and growing
               conditions, making them both beautiful and wise gardening choices.
               Use the tools below to find plants best suited to your tastes and
@@ -39,7 +32,7 @@ class InfographicDropdown extends React.Component {
             </h3>
           </div>
 
-          <div className={ "will-collapse" + (this.state.isCollapsed ? " is-collapsed" : "")}>
+          <div className={ "col col-sm-3 will-collapse" + (this.state.isCollapsed ? " is-collapsed" : "")}>
             <h2 style={{ fontWeight: "bold" }}>Why native plants?</h2>
             <h4>They need less water and fewer chemicals when established.</h4>
             <h4>
@@ -69,7 +62,7 @@ class InfographicDropdown extends React.Component {
           onClick={ this.onButtonClicked.bind(this) }>
           <img
             src="/images/garden/collapse-arrow.png"
-            className={ "will-v-flip" + (this.state.isCollapsed ? " v-flipped" : "") }
+            className={ "will-v-flip" + (this.state.isCollapsed ? " v-flip" : "") }
             style={{
               width: "4em",
               height: "4em",
@@ -83,11 +76,60 @@ class InfographicDropdown extends React.Component {
   }
 }
 
+function MainContentContainer(props) {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="col-sm-3 m-2 p-5 rounded-border" style={{ background: "green", minHeight: "20em" }}>
+
+      </div>
+    );
+  }
+}
+
+class SearchResultContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="col m-2 p-5 rounded-border" style={{ background: "blue", minHeight: "20em" }}>
+        <div className="row my-4 rounded-border" style={{ background: "green", minHeight: "5em" }}>
+        </div>
+        <div className="row my-4 rounded-border" style={{ background: "green", minHeight: "10em" }}>
+        </div>
+      </div>
+    );
+  }
+}
+
 class GardenPageApp extends React.Component {
   render() {
     return (
-      <InfographicDropdown />
-    )
+      <div>
+        <InfographicDropdown />
+        <MainContentContainer>
+          <SideBar />
+          <SearchResultContainer />
+        </MainContentContainer>
+      </div>
+    );
   }
 }
 
