512a610285d49d3097f3e4eef336f6c44d8b195b
Evin Dunn <evindunn14@gmail.com>
2019-09-06 14:44:44 -0700

    Finished 'plant needs' in react

--- js/garden/sidebar.jsx
+++ js/garden/sidebar.jsx
@@ -17,6 +17,9 @@ const searchButtonStyle = {
   background: "rgba(255, 255, 255, 0.5)"
 };
 
+/**
+ * Sidebar header with title, subtitle, and help
+ */
 function SideBarHeading(props) {
   return (
     <div style={{color: "black"}}>
@@ -37,6 +40,9 @@ function SideBarHeading(props) {
   );
 }
 
+/**
+ * Sidebar 'plant search' button
+ */
 function SearchButton(props) {
   return (
     <button className="my-auto" style={ searchButtonStyle } onClick={ props.onClick }>
@@ -53,7 +59,9 @@ function SearchButton(props) {
   );
 }
 
-
+/**
+ * Sidebar 'plant search' text field & button
+ */
 function SideBarSearch(props) {
   return (
     <div className="input-group w-100 mb-4 p-2">
@@ -61,18 +69,54 @@ function SideBarSearch(props) {
         name="search"
         type="text"
         placeholder="Search plants by name"
-        className="form-control search-param"
+        className="form-control"
         onChange={ props.onChange } />
       <SearchButton onClick={ props.onClick } showLoading={ props.isLoading } />
     </div>
   );
 }
 
+/**
+ * 'Plant Need' dropdown with label
+ */
+function PlantNeed(props) {
+  return (
+    <div className = "input-group pt-3 mt-3" style={{ borderTop: "1px dashed black" }}>
+      <label className="font-weight-bold" htmlFor={ props.label.toLowerCase() }>
+        { props.label }
+      </label>
+      <select
+        id="sunlight"
+        name={ props.label.toLowerCase() }
+        className="form-control ml-auto"
+        style={{ maxWidth: "50%" }}
+        defaultValue=""
+        onChange={ props.onChange }>
+        <option value="" disabled hidden>Select...</option>
+        {
+          props.choices.map((opt) =>
+            <option key={ opt.toLowerCase() } value={ opt.toLowerCase() }>
+              { opt }
+            </option>
+          )
+        }
+      </select>
+    </div>
+  );
+}
+
+
+
+/**
+ * Full sidebar
+ */
 class SideBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchText: '',
+      sunlight: '',
+      moisture: '',
       isLoading: false
     }
   }
@@ -89,6 +133,16 @@ class SideBar extends React.Component {
     setTimeout(() => { this.setState({ isLoading: false }) }, 3000);
   }
 
+  onSunlightChanged() {
+    console.log("The current sunlight value is '" + event.target.value + "'");
+    this.setState({ sunlight: event.target.value });
+  }
+
+  onMoistureChanged() {
+    console.log("The current moisture value is '" + event.target.value + "'");
+    this.setState({ moisture: event.target.value });
+  }
+
   render() {
     return (
       <div id="sidebar" className="col-sm-3 m-2 p-5 rounded-border" style={{ background: "#DFEFD3", minHeight: "20em" }}>
@@ -98,6 +152,17 @@ class SideBar extends React.Component {
           onClick={ this.onSearch.bind(this) }
           isLoading={ this.state.isLoading }
         />
+        <div style={{ background: "white" }} className="rounded-border p-4">
+          <h4>Plant needs</h4>
+          <PlantNeed
+            label="Sunlight"
+            choices={ ["Sun", "Part-Shade", "Full-Shade"] }
+            onChange={ this.onSunlightChanged.bind(this) } />
+          <PlantNeed
+            label="Moisture"
+            choices={ ["Dry", "Moderate", "Wet"] }
+            onChange={ this.onMoistureChanged.bind(this) } />
+        </div>
       </div>
     );
   }
