daff7537af4f444031a7aebf6971fd92e17562f6
Arthur Parker <alp9000@gmail.com>
2022-04-29 14:32:33 -0700

    Pub - last revisions before production

--- pages/flora-of-oregon.php
+++ pages/flora-of-oregon.php
@@ -5,7 +5,7 @@ header( "Content-Type: text/html; charset=" . $charset );
 ?>
 <html>
 <head>
-    <title><?php echo $defaultTitle ?> How to get the most our of our site</title>
+    <title><?php echo $defaultTitle ?> Flora of Oregon</title>
     <meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="<?php echo $CLIENT_ROOT?>/css/base.css?<?php echo filemtime($SERVER_ROOT . '/css/base.css'); ?>">    
 		<link rel="stylesheet" type="text/css" href="<?php echo $CLIENT_ROOT?>/css/main.css?<?php echo filemtime($SERVER_ROOT . '/css/main.css'); ?>">   
@@ -36,21 +36,26 @@ header( "Content-Type: text/html; charset=" . $charset );
 
 						<div class="row">
 								<div id="column-main" class="col-lg-8">
-									<p>The Flora of Oregon is a three-volume reference that will be the state’s only flora published in the past half century and the first illustrated floristic work that exclusively addresses Oregon. Volumes 1 and 2 were published in 2015 and 2020, respectively, and can be purchased directly from the publisher, the <a href="https://shop.brit.org/Flora-of-Oregon-2?page=1" target="_blank">Botanical Research Institute of Texas Press</a> or from other vendors. Volume 3 has a projected publication date of early 2023. </strong></p>
+									<p>
+									The Flora of Oregon is a three-volume reference that will be the state’s only flora published in the past half century and the first illustrated floristic work that exclusively addresses Oregon. 
+									Volumes 1 and 2 were published in 2015 and 2020, respectively, and can be purchased directly from the publisher, the <a href="https://shop.brit.org/Flora-of-Oregon-2?page=1" target="_blank">Botanical Research Institute of Texas Press</a>, or from other vendors. 
+									Volume 3 has a projected publication date of 2023. Upon completion, we estimate detailed descriptions of 4,380 native and naturalized taxa will be published in the three volumes.
+									</p>
 									
 									<div class="volumes">
 									
 										<div class="description">
 											<p>Each volume is a beautifully illustrated regional flora that informs plant identification. Included are chapters and appendices providing essential context in topics such as ecology, habitats, gardening with natives, and insect-plant relationships.</p>
-											<img src="../images/pages/of-spread_7499-1200px.png" alt="Sample pages from Oregon Flora">
+											<img src="../pages/images/of-spread_7499-1200px.png" alt="Sample pages from Oregon Flora">
 										</div>
 										<div class="volume-detail">
 											<h2>Flora of Oregon Volume 1:</h2>
 											<h3>Pterodophytes, Gymnosperms, and Monocots</h3>
 											<div class="detail">
 												<img src="./images/flora_vol1.png" alt="Flora of Oregon Vol 1">
-												<p>The first comprehensive plant guide for the state in over 50 years, and the first that is illustrated, Volume 1 addresses the ferns, conifers, grasses, sedges, and lilies—1,054 taxa of native and naturalized plants. Plant identification keys, descriptions, and an Oregon map with the distribution for each taxon are provided. Pen and ink illustrations of 521 species are interspersed with the descriptions.</p>
-												<p>Chapters describe the state’s ecology and predominant plant habitats, 50 of the best places to see wildflowers, and biographical sketches of notable Oregon botanists. Appendices detail taxa restricted to a single ecoregion, endemics, and those not collected in more than 50 years.</p>
+												<p>Notable families in Volume 1 include all ferns and fern allies, conifers, sedges (Cyperaceae), grasses (Poaceae), orchids (Orchidaceae), and lilies. 
+												Front chapters are richly illustrated with color photographs; they describe the state’s ecology and predominant plant habitats, 50 of the best places to see wildflowers, and biographical sketches of notable Oregon botanists. 
+												Appendices detail taxa restricted to a single ecoregion, endemics, and those not collected in more than 50 years.</p>
 												<div class="cta"><a href="https://shop.brit.org/Flora-of-Oregon" target="_blank" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Purchase Volume 1</a></div>
 											</div>
 										</div>
@@ -59,10 +64,10 @@ header( "Content-Type: text/html; charset=" . $charset );
 											<h3>Dicots Aizoaceae - Fagaceae</h3>
 											<div class="detail">
 												<img src="./images/flora_vol2.png" alt="Flora of Oregon Vol 2">
-												<p>Upholding the unparalleled standards of quality and design established with the first volume, Volume 2 presents 1,668 taxa of Oregon dicots from families Aizoaceae through Fagaceae. Notable groups include the Asteraceae (representing >11% of the state’s flora), the mustard family (Brassicaceae), stonecrops (Crassulaceae), and the legumes (Fabaceae). The richly-illustrated front chapters cover gardening
-												with native plants and plant-insect interactions with a focus on butterflies and pollinators. Appendices list butterfly-foodplant pairs, pollinator specialists and their targeted plants, native garden plants that support insects, and features of native species used for gardening and landscaping.
-												</p>
-												<div class="cta"><a href="https://shop.brit.org/Flora-of-Oregon" target="_blank" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Purchase Volume 2</a></div>
+												<p>Notable families in Volume 2 include the sunflowers (Asteraceae), mustards (Brassicaceae), heaths (Ericaceae), and legumes (Fabaceae). 
+												Filled with photographs, the front chapters cover gardening with native plants and plant-insect interactions with a focus on butterflies and pollinators. 
+												Appendices list butterfly-foodplant pairs, pollinator specialists and their targeted plants, native garden plants that support insects, and features of native species used for gardening and landscaping.
+												<div class="cta"><a href="https://shop.brit.org/Flora-of-Oregon-V2" target="_blank" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Purchase Volume 2</a></div>
 											</div>
 										</div>
 										
@@ -72,12 +77,14 @@ header( "Content-Type: text/html; charset=" . $charset );
 								<div id="column-right" class="col-lg-4">
 									<div class="sidebox inset sponsor">
 										<h3>Help sponsor Volume 3!</h3>
-										<img src="../images/pages/flora_vol3.jpg" alt="Flora of Oregon Vol 3">
+										<img src="../pages/images/flora_vol3.jpg" alt="Flora of Oregon Vol 3">
 										<p>Become a part of this lasting legacy - help us complete the series by sponsoring a treatment or illustration for Volume 3 – Dicots: Garryaceae - Zygophyllaceae!</p>
 										<p>Get started by simply downloading our Oregon Flora Volume 3 sponsorship form:</p>
 										<div class="cta"><a href="pdfs/OregonFlora_Volume_3_sponsorship.pdf" download class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Help sponsor Volume 3</a></div>
 										
 									</div>
+									<?php
+									/*
 									<div class="sidebox inset updates">
 										<h3>Stay informed and keep your Flora up-to-date</h3>
 										<em>(Click an icon to download)</em>
@@ -100,10 +107,11 @@ header( "Content-Type: text/html; charset=" . $charset );
 										
 										</ul>
 									</div>
+									*/
+									?>
 									<div class="sidebox praise">
 										<h2>Praise for <em>Flora of Oregon</em></h2>
-										<blockquote>
-											There is now a new standard of excellence for a state flora.
+										<blockquote>There is now a new standard of excellence for a state flora.
 										</blockquote>
 										<cite>
 											Neil Harriman, <em>Plant Science Bulletin</em> 61(4)
@@ -114,31 +122,25 @@ header( "Content-Type: text/html; charset=" . $charset );
 											Jenifer Penny & Daniel Brunton
 										</cite>
 										
-										<blockquote>
-										From start (fascinating introductory materials) to finish (information-rich appendices) and everything in between (inspired treatments), this is a winner.
+										<blockquote>From start (fascinating introductory materials) to finish (information-rich appendices) and everything in between (inspired treatments), this is a winner.
 										</blockquote>
 										<cite>
 											Jenifer Penny & Daniel Brunton
 										</cite>
 										
 																			
-										<blockquote>
-										We can only hope Volume 3 is not far off so Flora of Oregon can assume its position as the new standard of excellence for the production of regional North American floras.
+										<blockquote>We can only hope Volume 3 is not far off so Flora of Oregon can assume its position as the new standard of excellence for the production of regional North American floras.
 										</blockquote>										
 										<cite>
 											Jenifer Penny & Daniel Brunton,<br><em>Canadian Field Naturalist</em> v.135(1)
 										</cite>									
 										 
-										<blockquote>
-										Page layouts for species treatments strikes a good visual balance between illustrations, maps and text. I was struck by how usable this format is, compared to many older floras.
-										</blockquote>
+										<blockquote>Page layouts for species treatments strikes a good visual balance between illustrations, maps and text. I was struck by how usable this format is, compared to many older floras.</blockquote>
 										<cite>
 											Kathleen Sayce, <em>Douglasia</em> 45(3)
 										</cite>
 										 
-										<blockquote>
-										 Contemporary, authoritative, comprehensive, well-illustrated, beautifully laid-out, and easy-to-use.
-										</blockquote>
+										<blockquote>Contemporary, authoritative, comprehensive, well-illustrated, beautifully laid-out, and easy-to-use.</blockquote>
 										<cite>
 											David Giblin, <em>Native Plant Society of Oregon 2015 Book Review</em> 
 										</cite>
