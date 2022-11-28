4a1c9be5942660af05775cae89b3b163aa5a7bb6
Arthur Parker <alp9000@gmail.com>
2022-11-28 13:31:19 -0800

    Correct api

--- checklists/rpc/api.php
+++ checklists/rpc/api.php
@@ -19,13 +19,11 @@ function getEmpty() {
     "iconUrl" => '',
     "authors" => '',
     "abstract" => '',
-    "latcentroid" => 0,
-    "longcentroid" => 0,
-    'locality'=>'',
-		'publication'=>'',
 		'notes'=>'',
-		'pointradiusmeters'=> 0,
-    "taxa" => []
+    "lat" => 0,
+    "lng" => 0,
+    "taxa" => [],
+    "tids" => []
   ];
 }
 
@@ -40,7 +38,6 @@ function buildResult($checklistObj) {
   		$project = InventoryManager::fromModel($model);
   		$result["projName"] = $project->getProjname();
   	}
-  	  	
     $result["clid"] = $checklistObj->getClid();
     $result["title"] = $checklistObj->getTitle();
     $result["intro"] = ($checklistObj->getIntro()? $checklistObj->getIntro() :'') ;
@@ -73,6 +70,7 @@ function buildResult($checklistObj) {
 				#var_dump($vouchers);
 				$tjresult['vouchers'] = $vouchers[$rowArr['tid']];
 				$tjresult['sciname'] = $taxa->getSciname();
+				$tjresult['checklistNotes'] = $rowArr['checklistNotes'];
 				/*if (sizeof(explode(" ",$tjresult['sciname'])) == 1) {
 					$tjresult['sciname'] .= " sp.";#the old code does this, but Katie says it's unnecessary
 				}*/
@@ -137,90 +135,8 @@ function buildDynResult($dynclid) {
   return $result;
 }
 
-function updateInfo($model) {
-	$em = SymbosuEntityManager::getEntityManager();
-	$result = [];
-	$fields = array(
-		'name'=>'setName',
-		'authors'=>'setAuthors',
-		'locality'=>'setLocality',
-		'publication'=>'setPublication',
-		'abstract'=>'setAbstract',
-		'notes'=>'setNotes',
-		'latcentroid'=>'setLatcentroid',
-		'longcentroid'=>'setLongcentroid',
-		'pointradiusmeters'=>'setPointradiusmeters'
-	);
-	$success = 0;
-	$error = 0;
-	foreach ($fields as $field => $function) {
-		if (isset($_GET[$field]) && method_exists($model,$function)) {
-			$model->$function($_GET[$field]);
-			$success++;
-		}
-	}
-	$em->persist($model);
-	$em->flush();
-	$result = [
-		"success" => $success
-	];
-	
-	return $result;
-}
-function updateSPP() {
-	$result = [];
-	$success = 0;
-	$error = 0;
-	
-	if (array_key_exists("spp", $_GET) && array_key_exists("action", $_GET) ) {
-		if ($_GET['action'] == 'add') {
-			foreach ($_GET['spp'] as $tid) {
-				try {
-					/*
-						merge will be deprecated in future versions.  
-						Using persist can cause a duplicate key error, 
-						which should be handled by try/catch, but isn't.
-						So for now, we stick with merge.
-					*/
-  				$em = SymbosuEntityManager::getEntityManager();
-					$repo = new Fmchklsttaxalink();
-					$repo->setTid($tid);
-					$repo->setClid($_GET['clid']);
-					$repo->setInitialtimestamp(new \DateTime());
-					$em->merge($repo);#persist
-					$em->flush();
-					$success++;
-				}
-				catch (UniqueConstraintViolationException $e) {
-					#SymbosuEntityManager::resetManager();
-				}
-			}
-		}elseif($_GET['action'] == 'delete') {
-			foreach ($_GET['spp'] as $tid) {
-  			$em = SymbosuEntityManager::getEntityManager();
-  			$repo = $em->getRepository("Fmchklsttaxalink");
-				$link = $repo->find([
-					'tid' => $tid,
-					'clid' => $_GET['clid'],
-					'morphospecies' => ''
-				]);
-				$em->remove($link);
-				$em->flush();
-				$success++;
-			}
-		}
-	}
-	$result = [
-		"success" => $success
-	];
-	#var_dump($result);
-	#exit;
-	return $result;
-}
-
 $result = [];
-
-if (array_key_exists("clid", $_GET) && is_numeric($_GET["clid"])&& array_key_exists("pid", $_GET) && is_numeric($_GET["pid"])) {
+if (array_key_exists("clid", $_GET) && $_GET["clid"] > -1 && array_key_exists("pid", $_GET) && $_GET["pid"] > -1) {
   $em = SymbosuEntityManager::getEntityManager();
   $repo = $em->getRepository("Fmchecklists");
   $model = $repo->find($_GET["clid"]);
@@ -229,33 +145,22 @@ if (array_key_exists("clid", $_GET) && is_numeric($_GET["clid"])&& array_key_exi
 	  $checklist->setPid($_GET["pid"]);
 	}
   
-  if (array_key_exists("update", $_GET)) {
-  	switch ($_GET['update']) {
-  		case 'info':
-				$result = updateInfo($model);
-				break;
-			case 'spp':
-				$result = updateSPP();
-				break;
-		}		
-	}else{
-  
-		if ( 	 ( array_key_exists("search", $_GET) && !empty($_GET["search"]) )
-				&& ( array_key_exists("name", $_GET) && in_array($_GET['name'],array('sciname','commonname')) )
-		) {
-			$checklist->setSearchTerm($_GET["search"]);
-			$checklist->setSearchName($_GET['name']);
+	if ( 	 ( array_key_exists("search", $_GET) && !empty($_GET["search"]) )
+			&& ( array_key_exists("name", $_GET) && in_array($_GET['name'],array('sciname','commonname')) )
+	) {
+		$checklist->setSearchTerm($_GET["search"]);
+		$checklist->setSearchName($_GET['name']);
 		
-			$synonyms = (isset($_GET['synonyms']) && $_GET['synonyms'] == 'on') ? true : false;
-			$checklist->setSearchSynonyms($synonyms);
-		}
-		#$test = $checklist->getPid();
-		#var_dump($test);
-		$result = buildResult($checklist);
+		$synonyms = (isset($_GET['synonyms']) && $_GET['synonyms'] == 'on') ? true : false;
+		$checklist->setSearchSynonyms($synonyms);
 	}
+	$result = buildResult($checklist);
+
 }elseif(array_key_exists("dynclid", $_GET) && $_GET["dynclid"] > -1) {
 	$dynclid = $_GET["dynclid"];
 	$result = buildDynResult($dynclid);
+
+	
 }else{
 	#todo: generate error or redirect
 }
@@ -267,8 +172,4 @@ header("Content-Type: application/json; charset=utf-8");
 echo json_encode($result, JSON_NUMERIC_CHECK | JSON_INVALID_UTF8_SUBSTITUTE);
 
 
-
-
-
-?>
 
