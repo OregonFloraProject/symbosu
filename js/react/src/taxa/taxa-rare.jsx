a84995ebb76f56ad7d72747f278f15aa4ec4a0e0
Eric Samelson <samelsoe@oregonstate.edu>
2024-07-24 18:05:32 -0700

    [js][wip] start on some basic styling for taxa-rare

--- js/react/src/taxa/taxa-rare.jsx
+++ js/react/src/taxa/taxa-rare.jsx
@@ -6,14 +6,13 @@ import ImageModal from "../common/modal.jsx";
 import httpGet from "../common/httpGet.js";
 import { getUrlQueryParams } from "../common/queryParams.js";
 import { getTaxaPage } from "../common/taxaUtils";
-import ExplorePreviewModal from "../explore/previewModal.jsx";
 
 function showItem(item) {
   const isArray = Array.isArray(item);
   return (!isArray && item !== '') || item.length > 0;
 }
 
-// TODO(eric): extract these 4 components out from here and taxa-garden
+// TODO(eric): extract these components out from here and taxa-garden?
 function BorderedItem(props) {
   let value = props.value;
   const isArray = Array.isArray(value);
@@ -24,33 +23,14 @@ function BorderedItem(props) {
         { props.value.map((v) => <li key={ v }>{ v }</li>) }
       </ul>
     );
-  }
-
-  return (
-    <div className={ "row dashed-border" }>
-      <div className="col px-0 font-weight-bold char-label">{ props.keyName }</div>
-      <div className="col px-0 char-value">{ value }</div>
-    </div>
-  );
-}
-
-function BorderedItemVendor(props) {
-  let value = props.value;
-  const isArray = Array.isArray(value);
-
-  if (isArray) {
+  } else if (typeof value === 'object') {
     value = (
       <ul className="list-unstyled p-0 m-0">
-        { props.value.map((v) => {
-        		return (
-        			<li key={ v.clid }><a href={ props.clientRoot + '/checklists/checklist.php?cl=' + v.clid + '&pid=4' }>{ v.name }</a></li>
-        		)	 
-        	})
-      	}
-        
+        { Object.entries(props.value).map(([k, v]) => <li key={ k }>{ k } { v }</li>) }
       </ul>
-    );
+    )
   }
+
   return (
     <div className={ "row dashed-border" }>
       <div className="col px-0 font-weight-bold char-label">{ props.keyName }</div>
@@ -80,26 +60,96 @@ function SideBarSection(props) {
   );
 }
 
-function SideBarSectionVendor(props) {
-  let itemKeys = Object.keys(props.items);
-  itemKeys = itemKeys.filter((k) => {
-    const v = props.items[k];
-    return showItem(v);
-  });
-
+function SideBarSectionList(props) {
+  const { items } = props
   return (
-      <div className={ "sidebar-section mb-4 " + (itemKeys.length > 0 ? "" : "d-none") }>
+    <div className={ "sidebar-section mb-4 " + (items.length > 0 ? "" : "d-none") }>
+      <h3 className="text-light-green font-weight-bold mb-3">{ props.title }</h3>
+      <span className="row dashed-border"/>
+      {
+        items.map((val) => {
+          return (
+            <div className="associated-sciname" key={val}>{val}</div>
+          );
+        })
+      }
+      <span className="row dashed-border" />
+    </div>
+  )
+}
+
+function LookalikesTableRow(props) {
+  return (
+    <div className={`row dashed-border${props.isHeader ? ' is-header' : ''}`}>
+      <div className={`col px-0 char-label${props.isRow ? ' lookalike-sciname' : ''}`}>{ props.keyName }</div>
+      <div className="col px-0 char-value">{ props.value }</div>
+    </div>
+  );
+}
+
+function SideBarSectionTable(props) {
+  const { items } = props;
+  return (
+      <div className={ "sidebar-section mb-4 " + (items.length > 0 ? "" : "d-none") }>
         <h3 className="text-light-green font-weight-bold mb-3">{ props.title }</h3>
-        {
-          itemKeys.map((key) => {
-            const val = props.items[key];
-            return <BorderedItemVendor key={ key } keyName={ key } value={ val } clientRoot={ props.clientRoot } />
-          })
-        }
+        <LookalikesTableRow isHeader keyName="Taxon" value="Differs from featured plant by" />
+        {items.map(({ taxon, description }) => (
+          <LookalikesTableRow isRow keyName={taxon} value={description} />
+        ))}
         <span className="row dashed-border"/>
     </div>
   );
-} 
+}
+
+const dummyData = {
+  vernacularNames: ['Pale larkspur'],
+  sciName: 'Delphinium leucophaeum',
+  context: {
+    family: 'Ranunculaceae',
+    status: {
+      ranking: 'G2/S2',
+      federal: 'Species of concern',
+      state: 'Endangered',
+      orbic: 'List 1',
+    },
+    ecoregion: 'Willamette Valley',
+    counties: ['Clackamas', 'Multnomah', 'Washington'],
+    habitat: ['rock/cliff/scree/talus/bare ground', 'urban/agricultural/roadsides/human disturbance sites'],
+    elevation: '15 - 320 meters',
+    floweringTime: ['April', 'May'],
+  },
+  surveyManage: {
+    bestSurveyStatus: 'flowering',
+    bestSurveyTime: 'late May - early August',
+    threats: ['development', 'habitat loss', 'hybridization', 'invasive species'],
+    management: ['habitat preservation/conservation'],
+  },
+  lookalikes: [
+    { taxon: 'Delphinium pavoneaceum', description: 'taller; larger flower parts; sepals +/- reflexed to spreading, lower petals with hairy tuft at base of blade, inflorescence raceme wider below, narrowed above' },
+    { taxon: 'D. nuttallii', description: 'blue sepals' },
+  ],
+  associatedSpecies: [
+    'Anthoxanthum odoratum',
+    'Aquilegia formosa',
+    'Arbutus menziesii',
+    'Bromus',
+    'Camassia quamash',
+    'Collinsia parviflora',
+    'etc.',
+  ],
+  description: {
+    plants: '3-6 dm tall, root fleshy, tuberous.<br /><br />Stems usually single, slender, or rarely thickened, easily detached from tuber, puberulent.',
+    leaves: 'principally cauline, the lower ones withered by flowering time, blades glabrous or puberulent, 2-7 cm wide, 1-3 times dissected, ultimate segments linear to oblanceolate or broadly elliptic, margins smooth.',
+    inflorescences: 'simple of with several axillary branches below the terminal raceme, 1-20 flowers per stem and branch, pedicels 3/4-6 times the length of calyx spur, erect to spreading, puberulent.',
+    flowers: 'sepals light yellow or white with bluish tips, spreading, 8-15 mm long, spur 9-11 mm. Lower petals white, 4-6 mm, emarginate or cleft up to 1 mm.',
+    fruits: 'follicles erect, 10-15 mm, glabrous or puberulent.',
+    seeds: 'wing-margined.<br /><br />2n = 16.<br /><br />River bluffs, cliffs, talus, rocky slopes and meadows, roadsides, low elevations. WV. WA. Native.',
+  },
+  habitatNotes: '<i>Delphinium leucophaeum</i> grows on the edges of oak woodlands, often associated with rocky, gravelly areas such as roadside ditches, rocky slopes, where materials accumulate from landslides or other erosion activities, and lowland meadows. Plants are observed in shallow soils high in organic matter and sand. Occurrences encompass a gradient of slopes and exposures, from flat areas to steep slopes, in full sun to relatively depp shade. Oregon\'s populations are restricted to the northern Willamette Valley in Clackamas, Marion, Multnomah, and Washington Counties. Further north, a small disjunct population can be found in Lewis County, Washington.',
+  ecologyNotes: '<i>Delphinium leucophaeum</i> grows on the edges of oak woodlands, often associated with rocky, gravelly areas such as roadside ditches, rocky slopes, where materials accumulate from landslides or other erosion activities, and lowland meadows. Plants are observed in shallow soils high in organic matter and sand. Occurrences encompass a gradient of slopes and exposures, from flat areas to steep slopes, in full sun to relatively depp shade. Oregon\'s populations are restricted to the northern Willamette Valley in Clackamas, Marion, Multnomah, and Washington Counties. Further north, a small disjunct population can be found in Lewis County, Washington.',
+  conservationNotes: '<i>Delphinium leucophaeum</i> grows on the edges of oak woodlands, often associated with rocky, gravelly areas such as roadside ditches, rocky slopes, where materials accumulate from landslides or other erosion activities, and lowland meadows. Plants are observed in shallow soils high in organic matter and sand. Occurrences encompass a gradient of slopes and exposures, from flat areas to steep slopes, in full sun to relatively depp shade. Oregon\'s populations are restricted to the northern Willamette Valley in Clackamas, Marion, Multnomah, and Washington Counties. Further north, a small disjunct population can be found in Lewis County, Washington.',
+  literature: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br />https://www.npmjs.com/package/less-loader/v/5.0.0', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.']
+}
 
 function TaxaRareApp(props) {
   const [isLoading, setIsLoading] = useState(true);
@@ -254,8 +304,10 @@ function TaxaRareApp(props) {
     setIsPreviewOpen(!isPreviewOpen);
   }
 
+  const data = dummyData;
+
   const titleElement = document.getElementsByTagName("title")[0];
-  const pageTitle = `${props.defaultTitle} ${sciName}`
+  const pageTitle = `${props.defaultTitle} ${data.sciName}`
   titleElement.innerHTML = pageTitle;
   return (
     <div className="container mx-auto pl-4 pr-4 pt-5" style={{ minHeight: "45em" }}>
@@ -269,8 +321,8 @@ function TaxaRareApp(props) {
       </div>
       <div className="row print-start">
         <div className="col">
-          <h1 className="">{ vernacularNames[0] }</h1>
-          <h2 className="font-italic">{ sciName }</h2>
+          <h1 className="">{ data.vernacularNames[0] }</h1>
+          <h2 className="font-italic">{ data.sciName }</h2>
         </div>
         <div className="col-auto">
           <button className="d-block my-2 btn-primary print-trigger" onClick={() => window.print()}>Print page</button>
@@ -286,24 +338,16 @@ function TaxaRareApp(props) {
                 <img
                   id="img-main"
                   src={ images[0].url }
-                  alt={ sciName }
+                  alt={ data.sciName }
                 />
               </div>
-            <figcaption>{ images[0].photographer}</figcaption>
+            <figcaption>{ images[0].photographer }</figcaption>
             </figure>
           }
-          
-          <p className="mt-4">
-            {/*
-              Description includes HTML tags & URL-encoded characters in the db.
-              It's dangerous to pull/render arbitrary HTML w/ react, so just render the
-              plain text & remove any HTML in it.
-            */}
-            { description.replace(/(<\/?[^>]+>)|(&[^;]+;)/g, "") }
-          </p>
+
           <div className="mt-4 dashed-border taxa-slideshows">
           
-            <h3 className="text-light-green font-weight-bold mt-2">{ vernacularNames[0] } images</h3>
+            <h3 className="text-light-green font-weight-bold mt-2">{ data.vernacularNames[0] } images</h3>
             <div className="slider-wrapper">
             <ImageCarousel
               images={images}
@@ -334,6 +378,34 @@ function TaxaRareApp(props) {
             </div>
 
           </div>
+          
+          <div className="mt-4 dashed-border taxa-prose">
+            <h2>Summary</h2>
+            <p>
+              {/*
+                Description includes HTML tags & URL-encoded characters in the db.
+                It's dangerous to pull/render arbitrary HTML w/ react, so just render the
+                plain text & remove any HTML in it.
+              */}
+              <span class="taxa-prose-section-title">Habitat & distribution</span>{ data.habitatNotes.replace(/(<\/?[^>]+>)|(&[^;]+;)/g, "") }
+            </p>
+            <p>
+              <span class="taxa-prose-section-title">Ecology, natural history & pollinator biology</span>{ data.ecologyNotes.replace(/(<\/?[^>]+>)|(&[^;]+;)/g, "") }
+            </p>
+            <p>
+              <span class="taxa-prose-section-title">Trends & conservation</span>{ data.conservationNotes.replace(/(<\/?[^>]+>)|(&[^;]+;)/g, "") }
+            </p>
+
+            <h2>Taxon description</h2>
+            {Object.entries(data.description).map(([key, value]) => (
+              <p><span class="taxa-prose-section-title">{key}</span>{ value.replace(/(<\/?[^>]+>)|(&[^;]+;)/g, "") }</p>
+            ))}
+
+            <h2>Relevant literature</h2>
+            {data.literature.map(entry => (
+              <p>{entry}</p>
+            ))}
+          </div>
         </div>
         <ImageModal 
           show={isOpen}
@@ -343,76 +415,20 @@ function TaxaRareApp(props) {
           clientRoot={ props.clientRoot }
         >
           <h3>
-            <span>{ vernacularNames[0] }</span> images
+            <span>{ data.vernacularNames[0] }</span> images
           </h3>
         </ImageModal>
         <div className="col-md-4 sidebar-section">
-          <SideBarSection title="Highlights" items={ highlights } />
-          { nativeGroups.length > 0 &&
+          <SideBarSection title="Context" items={ data.context } />
+
           <div className={ "mb-4 sidebar-canned" }>
-              <h3 className="text-light-green font-weight-bold mb-1">Native plant groups</h3>
-              <p>Containing <strong>{ vernacularNames[0] }:</strong></p>
-                <div className="canned-results dashed-border">
-                {
-                  nativeGroups.map((checklist) => {
-                                
-                    return (
-                      <div key={ checklist.clid } className={"py-2 canned-search-result"}>
-                        <h4 className="canned-title" onClick={() => togglePreviewModal(checklist.clid)}>{checklist.name}</h4>
-                        <div className="card" style={{padding: "0.5em"}}>
-                          <div className="card-body" style={{padding: "0"}}>
-                            <div style={{ position: "relative", width: "100%", height: "7em", borderRadius: "0.25em"}}>
-                              <img
-                                className="d-block"
-                                style={{width: "100%", height: "100%", objectFit: "cover"}}
-                                src={checklist.iconUrl}
-                                alt={checklist.description}
-                                onClick={() => togglePreviewModal(checklist.clid)}
-                                //onMouseOver={ this.onMouseOver }
-                              />
-                              {/*
-                              <div
-                                className="text-center text-sentence w-100 h-100 px-2 py-1 align-items-center"
-                                style={{
-                                  //display: hover ? "flex" : "none",
-                                  position: "absolute",
-                                  top: 0,
-                                  left: 0,
-                                  zIndex: 1000,
-                                  fontSize: "0.75em",
-                                  color: "white",
-                                  background: "rgba(100, 100, 100, 0.8)",
-                                  overflow: "hidden"
-                                }}
-                                onMouseOut={ this.onMouseOut }
-                              >
-                              </div>
-                              */}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )
-                  })
-                }
-                </div>
-                <span className="row mt-2 dashed-border"/>						
-                <ExplorePreviewModal 
-                  key={currClid}
-                  show={isPreviewOpen}
-                  onTogglePreviewClick={togglePreviewModal}
-                  clid={currClid}
-                  pid={currPid}
-                  clientRoot={props.clientRoot}
-                  referrer={ 'taxa-garden' } 
-                ></ExplorePreviewModal>
-              </div>
+            <h3 className="text-light-green font-weight-bold mb-1">Distribution</h3>
+            <span className="row mt-2 dashed-border"/>
+          </div>
 
-            }
-
-          <SideBarSection title="Plant Facts" items={ plantFacts } />
-          <SideBarSection title="Growth and Maintenance" items={ growthMaintenance } />
-          <SideBarSectionVendor title="Commercial Availability" items={ commercialAvailability } clientRoot={props.clientRoot}/>
+          <SideBarSection title="Survey & Manage" items={ data.surveyManage } />
+          <SideBarSectionTable title="Look-Alikes" items={ data.lookalikes } />
+          <SideBarSectionList title="Associated species" items={ data.associatedSpecies } />
           <div className="taxa-link">
             <a href={ getTaxaPage(props.clientRoot, tid) }><button className="d-block my-2 btn-primary">Core profile page</button></a>
           </div>
