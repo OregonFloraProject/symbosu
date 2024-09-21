2160770a0666994afa4bf50b6c6d56f4b8b0a3f2
Eric Samelson <eric.samelson@oregonstate.edu>
2024-09-20 17:21:58 -0700

    final styling tweaks before feedback

--- js/react/src/rare/policy.jsx
+++ js/react/src/rare/policy.jsx
@@ -143,121 +143,121 @@ function PolicyApp(props) {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas sem id lectus ultricies sollicitudin. Aliquam tristique turpis ac ipsum rutrum ornare.</p>
           <p>Sed congue consectetur venenatis. Ut blandit tellus nisi, et rhoncus purus blandit quis. Nunc dignissim quam nisi, id dictum nisl accumsan et. Nullam ut euismod tortor. Suspendisse accumsan erat tortor, sit amet aliquam purus luctus eu. Proin libero nisl, auctor non efficitur at, placerat vel odio.</p>
           <p>Mauris dapibus finibus augue, a posuere mauris consequat non. Duis volutpat fermentum imperdiet. Curabitur nec ex eros. </p>
-          <hr />
-          {!shouldShowForm ? (
-            isLoggedIn ?
-              <div>{StatusDisplayText[status]}</div> :
-              <button
-                className="btn-primary"
-                href={`${props.clientRoot}/profile/index.php?refurl=${ location.pathname }`}
-              >
-                Login to request access
-              </button>
-            ) :
-            <div className="pb-4">
-              <h5>
-                To request access to restricted data, please fill out the following form:
-              </h5>
-              <form>
-                <FormElement
+          <div className="py-4">
+            {!shouldShowForm ? (
+              isLoggedIn ?
+                <h5>{StatusDisplayText[status]}</h5> :
+                <a href={`${props.clientRoot}/profile/index.php?refurl=${ location.pathname }`}>
+                  <button className="btn-primary">
+                    Login to request access
+                  </button>
+                </a>
+              ) :
+              <>
+                <h5>
+                  To request access to restricted data, please fill out the following form:
+                </h5>
+                <form>
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="firstName"
+                    value={firstName}
+                    onChange={setFirstName}
+                    label="First Name"
+                    validation={validation.firstName}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="lastName"
+                    value={lastName}
+                    onChange={setLastName}
+                    label="Last Name"
+                    validation={validation.lastName}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={setEmail}
+                    label="Email"
+                    validation={validation.email}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={setTitle}
+                    label="Position / Role"
+                    validation={validation.title}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="institution"
+                    value={institution}
+                    onChange={setInstitution}
+                    label="Institution / Agency"
+                    validation={validation.institution}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="text"
+                    id="department"
+                    value={department}
+                    onChange={setDepartment}
+                    label="Department (optional)"
+                    validation={validation.department}
+                  />
+                  <FormElement
+                    disabled={isLoading}
+                    type="textarea"
+                    maxLength={250}
+                    id="reason"
+                    value={reason}
+                    onChange={setReason}
+                    label="Reason for requesting access"
+                    validation={validation.reason}
+                  />
+                </form>
+                <button
+                  className="btn-primary"
                   disabled={isLoading}
-                  type="text"
-                  id="firstName"
-                  value={firstName}
-                  onChange={setFirstName}
-                  label="First Name"
-                  validation={validation.firstName}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="text"
-                  id="lastName"
-                  value={lastName}
-                  onChange={setLastName}
-                  label="Last Name"
-                  validation={validation.lastName}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="text"
-                  id="email"
-                  value={email}
-                  onChange={setEmail}
-                  label="Email"
-                  validation={validation.email}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="text"
-                  id="title"
-                  value={title}
-                  onChange={setTitle}
-                  label="Position / Role"
-                  validation={validation.title}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="text"
-                  id="institution"
-                  value={institution}
-                  onChange={setInstitution}
-                  label="Institution / Agency"
-                  validation={validation.institution}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="text"
-                  id="department"
-                  value={department}
-                  onChange={setDepartment}
-                  label="Department (optional)"
-                  validation={validation.department}
-                />
-                <FormElement
-                  disabled={isLoading}
-                  type="textarea"
-                  maxLength={250}
-                  id="reason"
-                  value={reason}
-                  onChange={setReason}
-                  label="Reason for requesting access"
-                  validation={validation.reason}
-                />
-              </form>
-              <button
-                className="btn-primary"
-                disabled={isLoading}
-                onClick={async () => {
-                  const data = { firstName, lastName, email, title, institution, department, reason };
-                  const validation = validateForm(data);
-                  if (Object.keys(validation).length) {
-                    setValidation(validation);
-                  } else {
-                    setStatus(Status.LOADING);
-                    try {
-                      const res = await fetch(`${props.clientRoot}/profile/rpc/api.php`, {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/x-www-form-urlencoded",
-                        },
-                        body: postData(data),
-                      });
-                      if (!res.ok) {
-                        throw new Error(`Response status: ${res.status}`)
+                  onClick={async () => {
+                    const data = { firstName, lastName, email, title, institution, department, reason };
+                    const validation = validateForm(data);
+                    if (Object.keys(validation).length) {
+                      setValidation(validation);
+                    } else {
+                      setStatus(Status.LOADING);
+                      try {
+                        const res = await fetch(`${props.clientRoot}/profile/rpc/api.php`, {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                          },
+                          body: postData(data),
+                        });
+                        if (!res.ok) {
+                          throw new Error(`Response status: ${res.status}`)
+                        }
+                        setStatus(Status.SENT);
+                      } catch (e) {
+                        // TODO(eric): handle error
+                        console.error(e);
+                        setStatus(Status.ERROR);
                       }
-                      setStatus(Status.SENT);
-                    } catch (e) {
-                      // TODO(eric): handle error
-                      console.error(e);
-                      setStatus(Status.ERROR);
                     }
-                  }
-                }}
-              >
-                Update profile and submit request
-              </button>
-            </div>
-          }
+                  }}
+                >
+                  Update profile and submit request
+                </button>
+              </>
+            }
+          </div>
         </div>
       </section>
     </div>
