3764d4de346c686d164eef03d66a33bac395dd1d
Eric Samelson <eric.samelson@oregonstate.edu>
2024-10-01 13:48:33 -0700

    [js][taxa] clean up MapItem

--- js/react/src/taxa/components/MapItem.jsx
+++ js/react/src/taxa/components/MapItem.jsx
@@ -3,11 +3,8 @@ import React, { useState } from 'react';
 function MapItem(props) {
   const [showOverlay, setShowOverlay] = useState(false);
 
-	let mapImage = null;
-	mapImage = `${props.clientRoot}/images/maps/${props.tid}.jpg`;
-	// /map/googlemap.php?maptype=taxa&taxon=6076&clid=0
-	//let mapLink = `${props.clientRoot}/map/googlemap.php?maptype=taxa&clid=0&taxon=${props.tid}`;
-	let mapLink = `${props.clientRoot}/collections/map/googlemap.php?usethes=1&taxa=${props.tid}&minClusterSetting=10&gridSizeSetting=30`;
+	const mapImage = `${props.clientRoot}/images/maps/${props.tid}.jpg`;
+	const mapLink = `${props.clientRoot}/collections/map/googlemap.php?usethes=1&taxa=${props.tid}&minClusterSetting=10&gridSizeSetting=30`;
 
   const linkText = props.needsPermission ? 'Locality details restricted to authorized users' : 'Click/tap to launch';
   const onClickHandler = props.needsPermission ?
@@ -21,8 +18,7 @@ function MapItem(props) {
         <a
           className="map-link"
           onClick={onClickHandler}
-        >{/*
-          onClick={ () => window.open(mapLink,'gmap','toolbar=1,scrollbars=1,width=950,height=700,left=20,top=20') } */}
+        >
           <img
             src={mapImage}
             alt={props.title}
@@ -40,8 +36,7 @@ function MapItem(props) {
         <a
           className="map-link"
           onClick={onClickHandler}
-        >	{/*
-          onClick={ () => window.open(mapLink,'gmap','toolbar=1,scrollbars=1,width=950,height=700,left=20,top=20') } */}
+        >
           {linkText}
         </a>
 			</div>
