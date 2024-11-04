e23e1d5109828ccfcf506fbd1953e742caf0b891
Eric Samelson <eric.samelson@oregonstate.edu>
2024-11-04 11:33:38 -0800

    [js] add eslint and basic configuration

---
+++ js/react/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/prop-types": 0,
+      "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
