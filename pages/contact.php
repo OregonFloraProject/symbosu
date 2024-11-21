022265ef61fb2bf45ea6ae5efc81a263de330e76
Eric Samelson <eric.samelson@oregonstate.edu>
2024-11-21 12:00:33 -0800

    [pages][contact] add Eric to OF staff list

--- pages/contact.php
+++ pages/contact.php
@@ -85,7 +85,8 @@ Template for <a href="data/template-observation-submission.xlsx" download>observ
 							<a href="mailto:myersj8@oregonstate.edu">myersj8@oregonstate.edu</a>
 						</p>
             <p>
-            	Arthur Parker, software programmer<br />
+              Eric Samelson, software developer<br />
+              <a href="mailto:eric.samelson@oregonstate.edu">eric.samelson@oregonstate.edu</a>
 						</p>
 
           
