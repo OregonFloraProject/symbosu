OregonFloraProject/symbosu

commit eeae4c28ec410c4a0311e12b271928f3970a9d7a

[js] fix all eslint errors besides no-unused-vars; change to warning for now

js/react/src/identify/identify.jsx
@@ -469,8 +469,7 @@ class IdentifyApp extends React.Component {
   		}
   	});
   	this.setState({
-      filters: Object.assign({}, this.state.filters, { attrs: newAttrs }),
-      filters: Object.assign({}, this.state.filters, { sliders: newSliders }),
+      filters: Object.assign({}, this.state.filters, { attrs: newAttrs, sliders: newSliders }),
     });
   }
   sortResults(results) {//should receive taxa from API

js/react/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
   {
     rules: {
       "react/prop-types": 0,
-      "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
+      "no-unused-vars": ["warn", { "ignoreRestSiblings": true }],
     },
     settings: {
       react: {

js/react/src/common/crumbBuilder.jsx
@@ -8,7 +8,7 @@ function CrumbBuilder(props) {
           return (
             <span key={ crumb.title }>
               <a href={ crumb.url }>{ crumb.title }</a>
-              { idx !== props.crumbs.length - 1 ? <span className="d-inline-block mx-1">></span> : '' }
+              { idx !== props.crumbs.length - 1 ? <span className="d-inline-block mx-1">&gt;</span> : '' }
             </span>
           )
         })

js/react/src/common/formFields.jsx
@@ -26,7 +26,6 @@ export default class TextField extends React.Component {
 				type="text"
 				aria-label={this.state.name}
 				name={this.state.name}
-				section={this.state.section}
 				defaultValue={this.state.value}
 				//placeholder={this.state.placeholder}
 				onBlur={ this.handleBlur.bind(this)}

js/react/src/common/glossary.js
@@ -18,6 +18,7 @@ export function addGlossaryTooltips(text, glossary) {
     .join('|') + ')(es|s|ly)?\\b)', "gi");
 
   // Search the description for glossary matches, and add tooltip html to each one
+  // eslint-disable-next-line no-undef
   return DOMPurify.sanitize(text).replace(re, (match, group1, group2) => {
 
     // If no groups are captured, it's a word in an HTML tag (non-capturing group), so just return it as-is

js/react/src/common/helpButton.jsx
@@ -12,6 +12,7 @@ class HelpButton extends React.Component {
 
   componentDidMount() {
     const helpButtonId = this.getHelpButtonId();
+    // eslint-disable-next-line no-undef
     $(`#${helpButtonId}`).popover({
       title: this.props.title,
       html: true,

js/react/src/common/searchResults.jsx
@@ -237,7 +237,6 @@ class ExploreSearchResult extends React.Component  {
 														name={ this.props.sciName }
 														value={ this.props.sciName }	
 														action={ 'edit' }
-														tid={ this.props.tid } 				
 														title={ 'Edit Notes' }
 														onClick={ () => this.toggleEditingNotes()} 
 													>
@@ -252,7 +251,6 @@ class ExploreSearchResult extends React.Component  {
 														name={ this.props.sciName }
 														value={ this.props.sciName }	
 														action={ 'delete' }
-														tid={ this.props.tid } 				
 														title={ 'Delete from checklist' }
 														onClick={ () => this.props.storeChange({'action': 'delete','name': this.props.sciName, 'value': this.props.tid, 'section': 'spp'})} 
 													>
@@ -270,7 +268,6 @@ class ExploreSearchResult extends React.Component  {
 													<div>			
 														<textarea
 															aria-label="notes"
-															section="spp" 
 															name="notes" 
 															defaultValue={this.props.checklistNotes}
 															placeholder="Your notes here" 

js/react/src/common/textarea.jsx
@@ -9,7 +9,6 @@ export default class TextareaField extends TextField {
 			<textarea
 				aria-label={this.state.name}
 				name={this.state.name}
-				section={this.state.section}
 				defaultValue={this.state.value}
 				placeholder={this.state.placeholder}
 				onBlur={ this.handleBlur.bind(this)}

commit 0c20e6135d913a59728aff6d5fc523aec65d01ca

[js][ident] fix broken identify tool
Identify tool characteristic selection was broken by eeae4c28e, which unwittingly exposed broken code that was previously skipped; because filter.attrs keys are a combination of the cid and cs values, newCids never includes the full key, so newAttrs was always empty. Before eeae4c28e newAttrs was ignored, likely by accident. This commit fixes the issue for checkbox filters.

js/react/src/identify/identify.jsx
@@ -459,7 +459,7 @@ class IdentifyApp extends React.Component {
       });
     });
     Object.entries(this.state.filters.attrs).map(([cid, attr]) => {
-      if (newCids.indexOf(Number(cid)) != -1) {
+      if (newCids.indexOf(Number(cid.split('-')[0])) != -1) {
         newAttrs[cid] = attr;
       }
     });