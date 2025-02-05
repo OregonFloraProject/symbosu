ae4d7791593d2abdebbf929427a6ceac7702fce1
Eric Samelson <eric.samelson@oregonstate.edu>
2025-02-05 15:05:20 -0800

    [checklists][classes] refactor checklists API to use IdentManager for selecting all taxon data
    This is a big performance & efficiency win over using ExploreManager and TaxaManager to select all this data in separate queries per-taxon-per-field. This also makes the code paths for checklists and dynamic checklists more similar.

--- classes/ExploreManager.php
+++ classes/ExploreManager.php
@@ -13,9 +13,6 @@ class ExploreManager {
 	protected $pid;
   protected $taxa;
   protected $taxaVouchers;
-  protected $searchTerm;
-  protected $searchName = '';
-  protected $searchSynonyms = false;
 
   public function __construct($clid=-1) {
     if ($clid !== -1) {
@@ -129,117 +126,6 @@ class ExploreManager {
   }
   public function getPid() {
   	return $this->pid;
-  }
-  public function setSearchTerm($term) {
-  	$this->searchTerm = $term;
-  }
-  public function setSearchName($name = '') {
-  	if (in_array($name,array('sciname','commonname'))) {
-  		$this->searchName = $name;
-  	}
-  }
-  public function setSearchSynonyms($bool) {
-  	$this->searchSynonyms = ($bool === true? true: false);
-  }
-
-  
-  private function populateTaxa($clid) {
-  
-    $em = SymbosuEntityManager::getEntityManager();
-    $taxa = $em->createQueryBuilder();
-    $qb = $em->createQueryBuilder();
-  
-  	$leftJoins = array();
-  	$innerJoins = array();
-  	$wheres = array(); 
-  	$params = array();
-  	$orderBy = 't.sciname';
-  	
-  	$innerJoins[] = array("Fmchklsttaxalink", "ctl", "WITH", "t.tid = ctl.tid");
-  	#$innerJoins[] = array("Fmchecklists", "cl", "WITH", "ctl.clid = cl.clid");
-  	$innerJoins[] = array("Taxstatus", "ts", "WITH", "t.tid = ts.tid");
-  	
-  	$wheres[] = "ctl.clid = :clid";
-  	#$wheres[] = "cl.parentclid = 1";
-  	$wheres[] = "ts.taxauthid = 1";
-  	
-  	$params[] = array(":clid",$clid);
-  	if ($this->pid > -1) {
-  		$leftJoins[] = array("Fmchklstprojlink", "cpl", "WITH", "ctl.clid = cpl.clid");
-  		$wheres[] = "cpl.pid = :pid";
-  		$params[] = array(":pid",$this->pid);
-  	}
-  	if ($this->searchTerm != '' && $this->searchName != '') {
-  		switch($this->searchName) {
-  			case 'commonname':
-  				$innerJoins[] = array("Taxavernaculars", "v", "WITH", "t.tid = v.tid");
-  				$params[] = array(":search",'%' . $this->searchTerm . '%');
-					if ($this->searchSynonyms) {
-						$wheres[] = $qb->expr()->orX(
-								$qb->expr()->like('v.vernacularname',':search'),
-								$qb->expr()->in(
-															"ts.tidaccepted",#array(1,2,3))
-															$em->createQueryBuilder()
-																->select("ts2.tidaccepted")
-																->from("Taxavernaculars","v2")
-																->innerJoin("Taxstatus","ts2","WITH","v2.tid = ts2.tid")
-																->where('v2.vernacularname LIKE :search')
-																->getDQL()
-															)
-						);
-					}else{
-	  				$wheres[] = "v.vernacularname LIKE :search";
-	  			}
-  				break;
-  			case 'sciname':
-  				$params[] = array(":search",'%' . $this->searchTerm . '%');
-  				if ($this->searchSynonyms) {
-						$wheres[] = $qb->expr()->orX(
-								$qb->expr()->like('t.sciname',':search'),
-								$qb->expr()->in(
-															"ts.tidaccepted",#array(1,2,3))
-															$em->createQueryBuilder()
-																->select("ts2.tidaccepted")
-																->from("Taxa","t2")
-																->innerJoin("Taxstatus","ts2","WITH","t2.tid = ts2.tid")
-																->where('t2.sciname LIKE :search')
-																->getDQL()
-															)
-						);
-					}else{
-	  				$wheres[] = "t.sciname LIKE :search";
-	  			}
-  				break;
-  		}
-  	}
-  
-    $taxa->select(["t.tid","COALESCE(ctl.familyoverride,ts.family) AS family","ctl.notes as checklistNotes"])//would like to use STRING_AGG here to concatenate with semicolons
-      ->from("Taxa", "t"); 
-        
-		foreach ($innerJoins as $innerJoin) {
-			$taxa->innerJoin(...$innerJoin);
-		}  
-		foreach ($leftJoins as $leftJoin) {
-			$taxa->leftJoin(...$leftJoin);
-		} 
-		if (sizeof($wheres)) {
-			foreach ($wheres as $where) {
-				$taxa->andWhere($where);
-			}
-		}
-		foreach ($params as $param) {
-			$taxa->setParameter(...$param);
-		}
-
-		$taxa->distinct();
-		$taxa->orderBy($orderBy);
-		
-		$tquery = $taxa->getQuery();
-		#var_dump($tquery->getSQL());exit;
-		$results = $tquery->execute();
-		#var_dump($results);exit;
-    return $results;
-
   }
 
   // JGM 2024-04-19: This code is deprecated in favor of a more efficient solution.
