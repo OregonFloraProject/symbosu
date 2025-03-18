1ce5006b5fc2618987aa7cabdbcb7bb29acfd7bf
Eric Samelson <eric.samelson@oregonstate.edu>
2025-03-18 14:48:46 -0700

    [pages] fix tutorials page by adding bootstrap JS to page head

--- pages/tutorials.php
+++ pages/tutorials.php
@@ -16,6 +16,12 @@ header( "Content-Type: text/html; charset=" . $charset );
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="video_modal.js"></script>
     <script src="https://kit.fontawesome.com/a01aa82192.js" crossorigin="anonymous"></script>
+    <script
+      type="text/javascript"
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous">
+    </script>
 </head>
 <body>
 <?php
