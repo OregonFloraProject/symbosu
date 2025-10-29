8831791dcdabad7fd67fbae045dc5fde6ff4d7c9
bachsofttrick <xuanbach1307@gmail.com>
2025-10-29 10:57:58 -0700

    Split synonyms and Misapplied Names in component

--- js/react/src/taxa/components/SynonymItem.jsx
+++ js/react/src/taxa/components/SynonymItem.jsx
@@ -1,67 +1,136 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export class SynonymItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showSynonyms: false,
-      //hiddenSynonyms: false,
+      showMisappliedNames: false,
       maxSynonyms: 3,
     };
   }
   toggleSynonyms = () => {
     this.setState({ showSynonyms: !this.state.showSynonyms });
   };
+  toggleMisappliedNames = () => {
+    this.setState({ showMisappliedNames: !this.state.showMisappliedNames });
+  };
 
   render() {
-    let visibleItems = this.props.value.slice(0, this.state.maxSynonyms);
-    let hiddenItems = this.props.value.slice(this.state.maxSynonyms);
+    const synonyms = [];
+    const misappliedNames = [];
+    this.props.value.forEach(synonym => {
+      if (synonym.notes === 'misapplied') {
+        misappliedNames.push(synonym);
+      } else {
+        synonyms.push(synonym);
+      }
+    });
+    const visibleSyns = synonyms.slice(0, this.state.maxSynonyms);
+    const visibleMisapplied = misappliedNames.slice(0, this.state.maxSynonyms);
+    const hiddenSyns = synonyms.slice(this.state.maxSynonyms);
+    const hiddenMisapplied = misappliedNames.slice(this.state.maxSynonyms);
 
     return (
-      <div className={'synonym-items row dashed-border py-1'}>
-        <div className="col font-weight-bold char-label">Synonyms and Misapplied Names</div>
-        <div className="synonym-list col">
-          <span className="short-list">
-            {visibleItems.length > 0 &&
-              Object.entries(visibleItems)
-                .map(([key, obj]) => {
-                  return (
-                    <span key={key} className={'synonym-item'}>
-                      <span className={'synonym-sciname'}>{obj.sciname}</span>
-                      <span className={'synonym-author'}> {obj.author}</span>
-                    </span>
-                  );
-                })
-                .reduce((prev, curr) => [prev, ', ', curr])}
-            {hiddenItems.length > 0 && !this.state.showSynonyms ? '...' : ''}
-          </span>
+      <>
+        {
+          synonyms.length > 0 ? (
+            <div className={'synonym-items row dashed-border py-1'}>
+              <div className="col font-weight-bold char-label">Synonyms</div>
+              <div className="synonym-list col">
+                <span className="short-list">
+                  {visibleSyns.length > 0 &&
+                    Object.entries(visibleSyns)
+                      .map(([key, obj]) => {
+                        return (
+                          <span key={key} className={'synonym-item'}>
+                            <span className={'synonym-sciname'}>{obj.sciname}</span>
+                            <span className={'synonym-author'}> {obj.author}</span>
+                          </span>
+                        );
+                      })
+                      .reduce((prev, curr) => [prev, ', ', curr])}
+                  {hiddenSyns.length > 0 && !this.state.showSynonyms ? '...' : ''}
+                </span>
 
-          <span className="full-list" hidden={!this.state.showSynonyms}>
-            {hiddenItems.length > 0 &&
-              Object.entries(hiddenItems)
-                .map(([key, obj]) => {
-                  return (
-                    <span key={key} className={'synonym-item'}>
-                      <span className={'synonym-sciname'}>{obj.sciname}</span>
-                      <span className={'synonym-author'}> {obj.author}</span>
-                    </span>
-                  );
-                })
-                .reduce((prev, curr) => [prev, ', ', curr])}
-          </span>
+                <span className="full-list" hidden={!this.state.showSynonyms}>
+                  {hiddenSyns.length > 0 &&
+                    Object.entries(hiddenSyns)
+                      .map(([key, obj]) => {
+                        return (
+                          <span key={key} className={'synonym-item'}>
+                            <span className={'synonym-sciname'}>{obj.sciname}</span>
+                            <span className={'synonym-author'}> {obj.author}</span>
+                          </span>
+                        );
+                      })
+                      .reduce((prev, curr) => [prev, ', ', curr])}
+                </span>
 
-          {this.props.value.length > this.state.maxSynonyms && (
-            <span>
-              <div className="up-down-toggle">
-                <FontAwesomeIcon
-                  icon={this.state.showSynonyms ? 'chevron-up' : 'chevron-down'}
-                  onClick={this.toggleSynonyms}
-                />
+                {synonyms.length > this.state.maxSynonyms && (
+                  <span>
+                    <div className="up-down-toggle">
+                      <FontAwesomeIcon
+                        icon={this.state.showSynonyms ? 'chevron-up' : 'chevron-down'}
+                        onClick={this.toggleSynonyms}
+                      />
+                    </div>
+                  </span>
+                )}
               </div>
-            </span>
-          )}
-        </div>
-      </div>
+            </div>
+          ): (<></>)
+        }
+        {
+          misappliedNames.length > 0 ? (
+            <div className={'synonym-items row dashed-border py-1'}>
+              <div className="col font-weight-bold char-label">Misapplied Names</div>
+              <div className="synonym-list col">
+                <span className="short-list">
+                  {visibleMisapplied.length > 0 &&
+                    Object.entries(visibleMisapplied)
+                      .map(([key, obj]) => {
+                        return (
+                          <span key={key} className={'synonym-item'}>
+                            <span className={'synonym-sciname'}>{obj.sciname}</span>
+                            <span className={'synonym-author'}> {obj.author}</span>
+                          </span>
+                        );
+                      })
+                      .reduce((prev, curr) => [prev, ', ', curr])}
+                  {hiddenMisapplied.length > 0 && !this.state.showMisappliedNames ? '...' : ''}
+                </span>
+
+                <span className="full-list" hidden={!this.state.showMisappliedNames}>
+                  {hiddenMisapplied.length > 0 &&
+                    Object.entries(hiddenMisapplied)
+                      .map(([key, obj]) => {
+                        return (
+                          <span key={key} className={'synonym-item'}>
+                            <span className={'synonym-sciname'}>{obj.sciname}</span>
+                            <span className={'synonym-author'}> {obj.author}</span>
+                          </span>
+                        );
+                      })
+                      .reduce((prev, curr) => [prev, ', ', curr])}
+                </span>
+
+                {misappliedNames.length > this.state.showMisappliedNames && (
+                  <span>
+                    <div className="up-down-toggle">
+                      <FontAwesomeIcon
+                        icon={this.state.showMisappliedNames ? 'chevron-up' : 'chevron-down'}
+                        onClick={this.toggleMisappliedNames}
+                      />
+                    </div>
+                  </span>
+                )}
+              </div>
+            </div>
+          ) : (<></>)
+        }
+      </>
     );
   }
 }
