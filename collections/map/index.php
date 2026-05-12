5d317e4b6babe7c41cacc2d424ef0612c456b25b
bachsofttrick <xuanbach1307@gmail.com>
2026-05-12 16:17:49 -0700

    temporary copy gettaxalinks.php into solrSearch getTaxaData

--- collections/map/index.php
+++ collections/map/index.php
@@ -1823,16 +1823,16 @@ if(isset($_REQUEST['llpoint'])) {
 
 <?php if ($USE_SOLR_SEARCH) { ?>
 				// get query string for sessionStorage and copy link button
-				const query = await fetch('rpc/searchCollections.php?queryOnly=true', {
+				const query = await (await fetch('rpc/searchCollections.php?queryOnly=true', {
 					method: 'POST',
 					body,
-				}).json();
+				})).json();
 				sessionStorage.querystr = query;
 
-				const response = await fetch('/spatial/rpc/solrSearch.php', {
+				const response = await (await fetch('../../spatial/rpc/solrSearch.php', {
 					method: 'POST',
 					body,
-				}).json();
+				})).json();
 				// if our query includes a polygon, save solrqString so we can pass it to downloadhandler
 				// since SOLR search is way faster than MySQL with polygons
 				if (body.has('polycoords') || body.has('upperlat')) {
